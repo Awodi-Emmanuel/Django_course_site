3c87aee0e66981367be9584a4622c3c91a282def
emmy70 <emmanuelusman701@gmail.com>
2021-11-14 20:23:54 +0100

    details page completed

--- meetups/urls.py
+++ meetups/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('meetups/', views.index) # our-domain.com/meetups
-
+    path('meetups/', views.index),  # our-domain.com/meetups
+    path('meetups/<slug:meetup_slug>', views.meetup_details),
+    # our-domain.com/meetups/a-second-meetiup/<dynamic-path-segment>
 ]
