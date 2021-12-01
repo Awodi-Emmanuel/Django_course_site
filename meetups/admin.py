95a0304f1f4a6c44863e38b1c4cebed5e9495b7c
emmy70 <emmanuelusman701@gmail.com>
2021-12-01 02:56:21 +0100

    fitching images from DB

--- meetups/admin.py
+++ meetups/admin.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 
-from .models import  Location, Meetups
+from .models import  Location, Meetups, Participant
 
 # Register your models here.
 class MeetupAdmin(admin.ModelAdmin):
     list_display = ('title', 'slug')
-    list_filter = ('title',)
+    list_filter = ('title', 'location',)
     prepopulated_fields = {'slug': ('title',)}
 
 
 admin.site.register(Meetups, MeetupAdmin)
 admin.site.register(Location)
+admin.site.register(Participant)
--- meetups/models.py
+++ meetups/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
 class Location(models.Model):
     name = models.CharField(max_length=200)
     address = models.CharField(max_length=300)
@@ -8,12 +9,21 @@ class Location(models.Model):
     def __str__(self):
         return f'{self.name} ({self.address})'
 
+
+class Participant(models.Model):
+    email = models.EmailField(unique=True)
+    # meetups = models.ManyToManyField(Meetup)
+
+    def __str__(self):
+        return self.email
+
 class Meetups(models.Model):
     title = models.CharField(max_length=200)
     slug = models.SlugField(unique=True)
     description = models.TextField()
     image = models.ImageField(upload_to='images')
     location = models.ForeignKey(Location, on_delete=models.CASCADE)
+    Participants = models.ManyToManyField(Participant, blank=True, null=True)
 
     def __str__(self):
         return f'{self.title} - {self.slug}'
